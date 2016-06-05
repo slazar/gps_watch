ce1a979d8978fc257d4349077122bbfb7f5c2bac
Sean Lazar <sean@squareup.com>
2016-06-05 14:15:42 -0700

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 # gps_watch
-Making a gps synched watch on the LinkIt ONE
+A gps synced clock/watch on the LinkIt ONE
+
+![display2](https://cloud.githubusercontent.com/assets/484713/15808170/aaeca472-2b25-11e6-804d-27dede649d4d.jpg)
+
+# Hardware
+
+* [Hackpack from Twilio Signal 2016 conference](http://hackpack.cc/)
+** [Seeed Studio LinkIt-One board and antennas](http://www.seeedstudio.com/item_detail.html?p_id=2017) 
+** [Lumi 128 x 64 OLED screen](http://www.seeedstudio.com/item_detail.html?p_id=781)
+** 3.7v 1050maH lithium ion battery
+
+# Features
+
+Shows time with seconds in GMT. Shows gps location in Decimal Degrees format, and gps satellite sync status.
+
+If you run it on battery, it will cycle the gps on and off to save power. Adjust the gpsOnPeriod and the gpsOffPeriod to suit your needs.
+
+Battery and charging state will show as icons in the bottom left hand corner of the screen.
+
+# Credits
+
+Code borrowed from:
+* [GPS-decimal_degrees.ino](https://github.com/MediaTek-Labs/linkIt-ONE-additional-example/blob/master/GPS-decimal_degrees/GPS-decimal_degrees.ino)
+* [deltainnovativelabs](https://deltainnovativelabs.wordpress.com/2015/01/15/linkit-one-rtc-example-code/)
+
+Big thanks to [Twilio Signal 2016 Conference](https://www.twilio.com/signal) for the sweet hackpack give away!
+
+# License
+
+Code written by me, battery and charging icons are free to use by anyone for commercial or private use.
+
+# More Images
+
+![photoshop](https://cloud.githubusercontent.com/assets/484713/15808044/593b520c-2b22-11e6-8342-8ec92838ea76.png)
+
+![draw](https://cloud.githubusercontent.com/assets/484713/15808045/5db2fac4-2b22-11e6-9577-96c600164007.png)
+
+![display](https://cloud.githubusercontent.com/assets/484713/15808046/64bf2964-2b22-11e6-9ea6-c73894f57fb1.jpg)
+
+
+
---
+++ gps_watch.ino
@@ -0,0 +1,225 @@
+#include <LDateTime.h>
+#include <LGPS.h>
+#include <LDateTime.h>
+#include <LBattery.h>
+#include <Wire.h>
+#include <SeeedOLED.h>
+#include "WatchIcons.h"
+
+datetimeInfo t;
+unsigned int rtc;
+unsigned int gpsLastChange=0;
+boolean gpsOn = false;
+gpsSentenceInfoStruct info;
+char format[60];
+const int gpsOnPeriod=300;   //adjust this for how long in seconds to keep gps on when on battery
+const int gpsOffPeriod=3600; //adjust this for how long in seconds to keep gps off when on battery
+
+void setup() {
+  Wire.begin();
+  SeeedOled.init();
+  SeeedOled.clearDisplay();
+  LGPS.powerOn(); //turn on gps for first sync
+  gpsOn = true;
+  LDateTime.getRtc(&gpsLastChange);
+}
+
+void printTime() {
+  char time_buffer[17];
+  LDateTime.getTime(&t);
+  LDateTime.getRtc(&rtc);
+  sprintf(time_buffer,"%02d:%02d:%02d GMT",t.hour,t.min,t.sec);
+  SeeedOled.setTextXY(0,0);
+  SeeedOled.putString(time_buffer);
+}
+
+boolean printGPGGA(char* str, char* GPS_formatted)
+{
+    char latitude[20];
+    char lat_direction[1];
+    char longitude[20];
+    char lon_direction[1];
+    char buf[20];
+    char time[30];
+    const char* p = str;
+    p = nextToken(p, 0); // GGA
+    p = nextToken(p, time); // Time
+    p = nextToken(p, latitude); // Latitude
+    p = nextToken(p, lat_direction); // N or S?
+    p = nextToken(p, longitude); // Longitude
+    p = nextToken(p, lon_direction); // E or W?
+    p = nextToken(p, buf); // fix quality
+    if (buf[0] == '1')
+    {
+      // GPS fix
+      p = nextToken(p, buf); // number of satellites
+      SeeedOled.setTextXY(4,0);
+      SeeedOled.putString(buf);
+      int sats = atoi(buf);
+      if (sats == 1)
+      {
+        SeeedOled.putString(" satellite     ");
+      }
+      else if (sats > 2 && sats < 10)
+      {
+        SeeedOled.putString(" satellites    ");        
+      }
+      else
+      {
+        SeeedOled.putString(" satellites   ");
+      }
+      
+      const int coord_size = 11;
+      char lat_fixed[coord_size],lon_fixed[coord_size];
+      convertCoords(latitude,longitude,lat_fixed, lon_fixed,coord_size);
+        
+      SeeedOled.setTextXY(2,0);
+      SeeedOled.putString(lat_fixed);
+      SeeedOled.putString(lat_direction);
+
+      SeeedOled.setTextXY(3,0);
+      SeeedOled.putString(lon_fixed);
+      SeeedOled.putString(lon_direction);
+
+      return true;
+    }
+    else
+    {
+      SeeedOled.setTextXY(4,0);
+      SeeedOled.putString("Acquiring GPS...");
+      return false;
+    }
+}
+
+void convertCoords(const char* latitude, const char* longitude, char* lat_return, char* lon_return, int buff_length)
+{
+    char lat_deg[3];
+    strncpy(lat_deg,latitude,2);      //extract the first 2 chars to get the latitudinal degrees
+    lat_deg[2] = 0;                   //null terminate
+     
+    char lon_deg[4];
+    strncpy(lon_deg,longitude,3);      //extract first 3 chars to get the longitudinal degrees
+    lon_deg[3] = 0;                    //null terminate
+     
+    int lat_deg_int = arrayToInt(lat_deg);    //convert to integer from char array
+    int lon_deg_int = arrayToInt(lon_deg);
+     
+    // must now take remainder/60
+    //this is to convert from degrees-mins-secs to decimal degrees
+    // so the coordinates are "google mappable"
+     
+    float latitude_float = arrayToFloat(latitude);      //convert the entire degrees-mins-secs coordinates into a float - this is for easier manipulation later
+    float longitude_float = arrayToFloat(longitude);
+     
+    latitude_float = latitude_float - (lat_deg_int*100);      //remove the degrees part of the coordinates - so we are left with only minutes-seconds part of the coordinates
+    longitude_float = longitude_float - (lon_deg_int*100);
+     
+    latitude_float /=60;                                    //convert minutes-seconds to decimal
+    longitude_float/=60;
+     
+    latitude_float += lat_deg_int;                          //add back on the degrees part, so it is decimal degrees
+    longitude_float+= lon_deg_int;
+     
+    snprintf(lat_return,buff_length,"%3.6f",latitude_float);    //format the coordinates nicely - no more than 6 decimal places
+    snprintf(lon_return,buff_length,"%3.6f",longitude_float);
+}
+ 
+int arrayToInt(const char* char_array)
+{
+    int temp;
+    sscanf(char_array,"%d",&temp);
+    return temp;
+}
+ 
+float arrayToFloat(const char* char_array)
+{
+    float temp;
+    sscanf(char_array, "%f", &temp);
+    return temp;
+}
+ 
+const char *nextToken(const char* src, char* buf)
+{
+    int i = 0;
+    while (src[i] != 0 && src[i] != ',')
+    i++;
+    if (buf)
+    {
+        strncpy(buf, src, i);
+        buf[i] = 0;
+    }
+    if (src[i])
+    i++;
+    return src + i;
+}
+
+void displayIcons()
+{
+  SeeedOled.setTextXY(7,0);
+  switch (LBattery.level()) {
+    case 0:
+      SeeedOled.drawBitmap(BatteryEmpty,16);
+      break;
+    case 33:
+      SeeedOled.drawBitmap(Battery33,16);
+      break;
+    case 66:
+      SeeedOled.drawBitmap(Battery66,16);
+      break;
+    case 100:
+      SeeedOled.drawBitmap(BatteryFull,16);
+      break;
+    default: 
+      SeeedOled.drawBitmap(BigSpace,16);
+    break;
+  }
+  SeeedOled.drawBitmap(LittleSpace,4);
+  if (LBattery.isCharging()) {
+    SeeedOled.drawBitmap(BatteryCharging,16);
+  }
+  else {
+    SeeedOled.drawBitmap(BigSpace,16);
+  }
+}
+
+void loop() {
+  printTime();
+  displayIcons();
+
+  if (gpsOn)
+  {
+    LGPS.getData(&info);
+    printGPGGA((char*)info.GPGGA,format);
+    if (LBattery.isCharging() == false)
+    {
+      if (rtc > gpsLastChange + gpsOnPeriod)
+      {
+        LGPS.powerOff();
+        gpsOn = false;
+        gpsLastChange = rtc;
+        SeeedOled.setTextXY(4,0);
+        SeeedOled.putString("GPS powered off ");
+      }
+    }
+  }
+  else
+  {
+    if (LBattery.isCharging() == false)
+    {
+      if (rtc > gpsLastChange + gpsOffPeriod)
+      {
+        LGPS.powerOn();
+        gpsOn = true;
+        gpsLastChange = rtc;
+      }
+    }
+    else
+    {
+      LGPS.powerOn();
+      gpsOn = true;
+      gpsLastChange = rtc;
+    }
+  }
+  
+  delay(1000);
+}
